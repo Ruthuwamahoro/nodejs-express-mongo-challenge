1724d39ea3f1abece9cd6beaef2edd583793ab87
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2024-11-16 19:39:27 +0200

    working on user auth

---
+++ my-rest-api/src/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {v4: uuidV4} = require('uuid');
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  gender: { type: String },
+  telephone: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema );
