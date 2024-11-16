1d846c73309762fc9da1b0947c7ca6b929ae5fc1
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2024-11-16 18:31:16 +0200

    handling errors by using try...catch

--- my-rest-api/src/routes/product.js
+++ my-rest-api/src/routes/product.js
@@ -3,39 +3,62 @@ const router = express.Router();
 const Product = require('../models/product');
 
 router.get('/', async (req, res) => {
-  const products = await Product.find();
-  res.json({status:200, message: "Successfully retrieved products", data: products})
+  try{
+    const products = await Product.find();
+    res.json({status:200, message: "Successfully retrieved products", data: products})
+  } catch(error){
+    res.json({status:500, message: "Unexpected error occured", data: error.message})
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  const singleProduct = await Product.findById(req.params.id);
-  if(singleProduct === null) res.status(400).json({status: 400, error: "id not found"})
-  res.json({status:200, message: "Successfully retrieved product", data: singleProduct}) 
+  try{
+    const singleProduct = await Product.findById(req.params.id);
+    if(singleProduct === null) res.status(400).json({status: 400, error: "id not found"})
+    res.json({status:200, message: "Successfully retrieved product", data: singleProduct})
+  } catch(error){
+    res.json({status:500, message: "Unexpected error occured", data: error.message})
+  }
 });
 
 router.post('/', async (req, res) => {
-  const {name , description, price} = await req.body
-  const products = new Product({
-    name,
-    description,
-    price
-  })
-  await products.save();
-  res.json({status: 200, message: "Successfully posted products", data: null})
+  try{
+    const {name , description, price} = await req.body
+    const products = new Product({
+      name,
+      description,
+      price
+    })
+    await products.save();
+    res.json({status: 200, message: "Successfully posted products", data: null})
+
+  } catch(error){
+    res.json({status:500, message: "Unexpected error occured", data: error.message})
+  }
+
 });
 
 router.put('/:id', async (req, res) => {
-  const newData = req.body
-  const updateProduct = await Product.findByIdAndUpdate(req.params.id, newData, {new: true});
-  if(updateProduct === null) res.status(400).json({status: 400, error: "id not found"})
-  res.status(200).json({status: "ok",data: updateProduct, message: "Successfully retrieved updated"});
+  try{
+    const newData = req.body
+    const updateProduct = await Product.findByIdAndUpdate(req.params.id, newData, {new: true});
+    if(updateProduct === null) res.status(400).json({status: 400, error: "id not found"})
+    res.status(200).json({status: "ok",data: updateProduct, message: "Successfully retrieved updated"});
+
+  } catch(error){
+    res.json({status:500, message: "Unexpected error occured", data: error.message})
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  const getId = req.params.id
-  const result = await Product.findByIdAndDelete(getId);
-  if(result === null) res.status(410).json({message: "product already deleted"})
-  res.json({status: "ok", data: null , message:"successfully deleted products"});
+  try{
+    const getId = req.params.id
+    const result = await Product.findByIdAndDelete(getId);
+    if(result === null) res.status(410).json({message: "product already deleted"})
+    res.json({status: "ok", data: null , message:"successfully deleted products"});
+  } catch(error){
+    res.json({status:500, message: "Unexpected error occured", data: error.message})
+  }
 });
 
 module.exports = router;
