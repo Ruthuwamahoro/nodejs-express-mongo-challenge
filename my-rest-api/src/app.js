73eb67482368e9b8f71e2171bd470bef3a2c3ed2
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2024-11-16 23:18:27 +0200

    working on auth

--- my-rest-api/src/app.js
+++ my-rest-api/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/product');
+const userRoutes = require('./routes/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ connectDB();
 app.use(express.json());
 
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- my-rest-api/src/utils/validateField.js
+++ my-rest-api/src/utils/validateField.js
@@ -1,5 +1,5 @@
-const { Joi } = require("joi");
-export const registerSchema = Joi.object({
+const  Joi  = require("joi");
+const registerSchema = Joi.object({
     email: Joi.string().email().required(),
     fullName: Joi.string().max(100).required(),
     gender: Joi.string().max(50).required(),
@@ -11,14 +11,17 @@ export const registerSchema = Joi.object({
         'any.only': 'Confirm password must be same as password'
     })
 })
-export const options = {
+const options = {
     abortEarly: false,
     allowUnknown: true,
     stripUnknown: true
 }
 
-export const ProductSchema = Joi.object({
+const ProductSchema = Joi.object({
     name: Joi.string().max(20).required(),
     description: Joi.string().min(4).max(15),
     price: Joi.number().min(4).max(15).required(),
 })
+
+
+module.exports = { registerSchema, options, ProductSchema}
