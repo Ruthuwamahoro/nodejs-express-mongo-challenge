cae770f84710450ce5e83761a1cb252cacc8be89
Beritha Niyotwagira <116920809+Beritha-n12@users.noreply.github.com>
2024-11-16 21:41:12 +0200

    working on CRUD products

--- my-rest-api/src/app.js
+++ my-rest-api/src/app.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const productRoutes = require('./routes/product');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-connectDB();
-
-app.use(express.json());
-
-app.use('/products', productRoutes);
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
